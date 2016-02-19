09633f6f00d7762ef4576a9c50f7d05acececd0d
jackb992 <jackbiles@outlook.com>
2016-02-19 17:27:46 +0000

    Update elf_to_cia_convertor.sh

--- Convert/elf_to_cia_convertor.sh
+++ Convert/elf_to_cia_convertor.sh
@@ -5,16 +5,19 @@ echo          ELF to CIA Convertor
 echo -Special Thanks to 1000pcrack provide some code-
 echo   Icon Image name must be "icon.png"
 echo   Banner Image name must be "bannner.png"
-echo   Wave faile name must be "audio.wav"
+echo   WAV audio name must be "audio.wav"
 echo ------------------------------------------
 read -n1 -r -p "Press any key to continue..."
 ./bannertool makebanner -i banner.png -a audio.wav -o banner.bnr
+echo Banner Made Succesfully!
 read -n1 -r -p
-echo Create icon.ico for cia
+echo  Create icon.ico for cia
 ./bannertool makesmdh -s “Decrypt9WIP” -l "Decrypt9WIP" -p "Archshift, d0k3, Shadowtrance & others" -i icon.png  -o icon.icn 
-echo finished Press any key!
+echo Icon made Succefully!
 read -n1 -r -p
 echo Building CIA
-./makerom -f cia -o install-me.cia -DAPP_ENCRYPTED=false -rsf AppInfo.rsf -target t -exefslogo -elf *.elf -icon icon.icn -banner banner.bnr
-echo Finished!
+./makerom -f cia -o Decrypt9WIP-CIA.cia -DAPP_ENCRYPTED=false -rsf AppInfo.rsf -target t -exefslogo -elf *.elf -icon icon.icn -banner banner.bnr
+echo CIA Made succesfully!
+echo Install Decrypt9WIP-CIA.cia
+read -n1 -r -p "Press any key to exit..."
 exit
